f8a49235cf3cf58fe3f80ce9f7c5cd9bb9326f26
Diego Mamprim Borges <egomamprim@gmail.com>
2022-09-16 21:07:17 -0300

    atualizações2

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      hello world!
+      hello world! <br />
       Herrar é umano
     </div>
   )
