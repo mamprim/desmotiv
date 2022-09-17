3fec0d1d121ee28d85b0b2d7af9007aebd5d8934
Diego Mamprim Borges <egomamprim@gmail.com>
2022-09-16 21:02:41 -0300

    atualizações

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      hello world! <br />
+      Herrar é umano
+    </div>
+  )
+}
+
+export default Home
